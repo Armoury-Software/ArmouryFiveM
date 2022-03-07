89b569f3a67abb1717c8d14e09314db73768b002
RaphaelCapone <44115221+RaphaelCapone@users.noreply.github.com>
2022-03-07 21:13:58 +0200

    Adding a new job
    Add a new job.

---
+++ garbage-man-job/src/shared/positions.ts
@@ -0,0 +1,31 @@
+import { Blip } from '../../../[utils]/models/blip.model';
+import { Marker, MarkerCircle } from '../../../[utils]/models/marker.model';
+
+import { GarbageManPoint } from './models/delivery-point.model';
+
+export const GARBAGE_MAN_CPOINTS: GarbageManPoint[] = [
+    {
+        pos: [-205.8197784423828,216.94944763183594,86.6871337890625],
+        count: 1,
+    },
+    {
+        pos: [-215.5120849609375,271.1736145019531,91.2535400390625],
+        count: 2,
+    },
+    {
+        pos: [-181.9912109375,248.00439453125,92.028564453125],
+        count: 3,
+    },
+    {
+        pos: [-115.89889526367188,217.7142791748047,94.16845703125],
+        count: 4,
+    },
+    {
+        pos: [143.4857177734375,206.57144165039062,106.16552734375],
+        count: 5,
+    },
+    {
+        pos: [138.09231567382812,166.1670379638672,104.2109375,-0.44406601786613464],
+        count: 6,
+    },
+];
