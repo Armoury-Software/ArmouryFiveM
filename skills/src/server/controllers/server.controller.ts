5f9e86b907d7fc9770fd5b3449d7613a396c6031
antoniost26 <91466692+antoniost26@users.noreply.github.com>
2022-03-09 22:10:11 +0200

    [AF-31] Added skill resource and updated truck job, playerbase

---
+++ skills/src/server/controllers/server.controller.ts
@@ -0,0 +1,82 @@
+import { ServerController } from '../../../../[utils]/server/server.controller';
+import { Skill } from '../../shared/models/skill.model';
+
+export class Server extends ServerController {
+    public constructor(){
+        super();
+
+        this.assignExports();
+        this.registerCommands();
+    }
+
+    private registerCommands(){
+        RegisterCommand('skills', (source: number) => {
+            global.exports['chat'].addMessage(source, '------------------Skills List------------------');
+
+            let playerSkills: Skill[] = this.getPlayerSkills(source);
+            if (!Array.isArray(playerSkills)) {
+                return;
+            }
+
+            playerSkills.forEach((skill: Skill) => {
+                global.exports['chat'].addMessage(source, skill.name + ': ' + skill.value);
+            })
+        }, false);
+    }
+
+    private updatePlayerSkill(playerId: number, skill: string, value: number): void {
+
+        const currentPlayerSkills: Skill[] = this.getPlayerSkills(playerId);
+
+        if (!currentPlayerSkills.find((_skill: Skill) => _skill.name === skill)) {
+            currentPlayerSkills.push({ name: skill, value });
+        }
+
+        const updatedPlayerSkills: Skill[] = currentPlayerSkills.map((_skill: Skill) => _skill.name === skill ? { ..._skill, value } : { ..._skill, value: _skill.value });
+        global.exports['authentication'].setPlayerInfo(playerId, 'skills', updatedPlayerSkills, false);
+    }
+
+    public getPlayerSkills(playerId: number): Skill[] {
+        let playerSkills: Skill[] = global.exports['authentication'].getPlayerInfo(playerId, 'skills');
+        
+
+        if (!Array.isArray(playerSkills)) {
+            playerSkills = [];
+        }
+
+        return playerSkills;
+    }
+
+    public getPlayerSkill(playerId: number, skill: string): Skill {
+        let playerSkills: Skill[] = this.getPlayerSkills(playerId);
+        let playerSkill: Skill = playerSkills.find((_skill: Skill) => _skill.name === skill);
+
+        if (!playerSkill) {
+            playerSkill = {
+                name: '',
+                value: 0
+            };
+        }
+
+        return playerSkill;
+    }
+
+    public incrementPlayerSkill(playerId: number, skill: string, value: number): void {
+        const playerSkill: Skill = this.getPlayerSkill(playerId, skill);
+
+        this.updatePlayerSkill(playerId, skill, Math.min(100, value + playerSkill.value));
+    }
+
+    public decrementPlayerSkill(playerId: number, skill:string, value: number): void {
+        const playerSkill: Skill = this.getPlayerSkill(playerId, skill);
+
+        this.updatePlayerSkill(playerId, skill, Math.max(0, value - playerSkill.value));
+    }
+
+    private assignExports(): void {
+        exports('getPlayerSkills', this.getPlayerSkills.bind(this));
+        exports('getPlayerSkill', this.getPlayerSkill.bind(this));
+        exports('incrementPlayerSkill', this.incrementPlayerSkill.bind(this));
+        exports('decrementPlayerSkill', this.decrementPlayerSkill.bind(this));
+    }
+}
---
+++ skills/webpack.config.js
@@ -0,0 +1,89 @@
+const webpack = require('webpack');
+const path = require('path');
+const RemovePlugin = require('remove-files-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const glob = require('glob');
+
+const buildPath = path.resolve(__dirname, 'dist');
+
+const server = {
+  context: __dirname,
+  entry: './src/server/server.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ['ts-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({ 'global.GENTLY': false }),
+    new RemovePlugin({
+      before: {
+        include: [
+          path.resolve(buildPath, 'server')
+        ]
+      },
+      watch: {
+        include: [
+          path.resolve(buildPath, 'server')
+        ]
+      }
+    }),
+    new ESLintPlugin()
+  ],
+  optimization: {
+    minimize: true,
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[contenthash].server.js',
+    path: path.resolve(buildPath, 'server')
+  },
+  target: 'node',
+};
+
+const client = {
+  context: __dirname,
+  entry: './src/client/client.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ['ts-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new RemovePlugin({
+      before: {
+        include: [
+          path.resolve(buildPath, 'client')
+        ]
+      },
+      watch: {
+        include: [
+          path.resolve(buildPath, 'client')
+        ]
+      }
+    }),
+    new ESLintPlugin()
+  ],
+  optimization: {
+    minimize: true,
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[contenthash].client.js',
+    path: path.resolve(buildPath, 'client'),
+  },
+};
+
+module.exports = [server, client];
