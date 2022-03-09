b6dbb240218eb0a78676bb279bebf49d5469017c
antoniost26 <91466692+antoniost26@users.noreply.github.com>
2022-03-09 21:31:43 +0200

    updated skill resource

--- skills/src/server/controllers/server.controller.ts
+++ skills/src/server/controllers/server.controller.ts
@@ -10,13 +10,13 @@ export class Server extends ServerController {
 
     private registerCommands(){
         RegisterCommand('skills', () => {
+            global.exports['chat'].addMessage(source, '------------------Skills List------------------');
+
             let playerSkills: Skill[] = this.getPlayerSkills(source);
 
             if (!Array.isArray(playerSkills)) {
                 return;
             }
-
-            global.exports['chat'].addMessage(source, '------------------Skills List------------------');
 
             playerSkills.forEach((skill: Skill) => {
                 global.exports['chat'].addMessage(source, skill.name + ': ' + skill.value);
@@ -32,13 +32,15 @@ export class Server extends ServerController {
             currentPlayerSkills.push({ name: skill, value });
         }
 
-        const updatedPlayerSkills: Skill[] = currentPlayerSkills.map((_skill: Skill) => _skill.name === skill ? { ..._skill, value } : { ..._skill, value: _skill.value })
-        global.exports['authentication'].setPlayerInfo(playerId, 'skills', updatedPlayerSkills, false)
+        const updatedPlayerSkills: Skill[] = currentPlayerSkills.map((_skill: Skill) => _skill.name === skill ? { ..._skill, value } : { ..._skill, value: _skill.value });
+        global.exports['authentication'].setPlayerInfo(playerId, 'skills', updatedPlayerSkills, false);
     }
 
     public getPlayerSkills(playerId: number): Skill[] {
         let playerSkills: Skill[] = global.exports['authentication'].getPlayerInfo(playerId, 'skills');
         
+        console.log(playerSkills);
+
         if (!Array.isArray(playerSkills)) {
             playerSkills = [];
         }
@@ -47,15 +49,23 @@ export class Server extends ServerController {
     }
 
     public getPlayerSkill(playerId: number, skill:string): Skill {
-        const playerSkills: Skill[] = this.getPlayerSkills(playerId);
+        let playerSkills: Skill[] = this.getPlayerSkills(playerId);
+        let playerSkill: Skill = playerSkills.find((_entity: Skill) => _entity.name === skill);
 
-        return playerSkills.find((_entity: Skill) => _entity.name === skill)
+        if (!playerSkill) {
+            playerSkill = {
+                name: '',
+                value: 0
+            };
+        }
+
+        return playerSkill;
     }
 
     public incrementPlayerSkill(playerId: number, skill: string, value: number): void {
         const playerSkill: Skill = this.getPlayerSkill(playerId, skill);
 
-        this.updatePlayerSkill(playerId, skill, Math.max(100, value + playerSkill.value));
+        this.updatePlayerSkill(playerId, skill, Math.min(100, value + playerSkill.value));
     }
 
     public decrementPlayerSkill(playerId: number, skill:string, value: number): void {
